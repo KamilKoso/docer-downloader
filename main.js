7c8a7c024b520f19ff4252ef1bb6bde41f8ae8b8
KamilKoso <kamilkosobudzki1@gmail.com>
2024-02-22 00:21:44 +0100

    Made it work for epub files. Improved the algorithm

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-const PDF_DOWNLOAD_URL_ATTRIBUTE = "data-pdf-url";
-const MUTATION_OBSERVER_CONFIG = {
-    attributes: true, 
-    attributeFilter: [PDF_DOWNLOAD_URL_ATTRIBUTE],
-    subtree: true
-}
+const NOT_READY_COLOR = "#c95145";
+const READY_COLOR = "#4caf50";
 
-const NOT_READY_COLOR = "#c95145"
-const READY_COLOR = "#4caf50"
-
-
-const downloadButton = document.getElementById("dwn_btn")
+const downloadButton = document.getElementById("dwn_btn");
 const pageContent = document.getElementById("content");
 
-function handleAttributeChange(mutationsList, observer) {
-    for(const mutation of mutationsList) {
-        console.log(mutation);
-        if (mutation.type === 'attributes' && mutation.attributeName === PDF_DOWNLOAD_URL_ATTRIBUTE) {
-           const downloadUrl = mutation.target.getAttribute(PDF_DOWNLOAD_URL_ATTRIBUTE);
-           downloadButton.onclick = () => {
-                window.open(downloadUrl, '_blank').focus();
-           };
-           downloadButton.style.borderColor = READY_COLOR;
-           observer.disconnect();
-        }
-    }
+
+async function getDownloadUrl() {
+    const item_id = downloadButton.getAttribute("data-id");
+    const formData = new FormData();
+    formData.append('item_id', item_id);
+    formData.append('rc', Math.floor((Math.random() * 9) + 1));
+
+    const result = await fetch("/start/download", {
+        method: "POST",
+        credentials: "include",
+        body: formData
+      });
+      
+      if(result.status !== 200) {
+        throw new Error(`Couldn't fetch download URL`, result);
+      }
+
+      return (await result.json()).response.url
 }
 
 
-function main() {
-    downloadButton.style.borderColor = NOT_READY_COLOR;
-    const observer = new MutationObserver(handleAttributeChange);
-    observer.observe(pageContent, MUTATION_OBSERVER_CONFIG)
+async function main() {
+  downloadButton.style.borderColor = NOT_READY_COLOR;
+  const downloadUrl = await getDownloadUrl();
+
+  downloadButton.onclick= () => {
+    window.open(downloadUrl, '_blank').focus();
+  };
+  downloadButton.style.borderColor = READY_COLOR;
 }
 
 main();
