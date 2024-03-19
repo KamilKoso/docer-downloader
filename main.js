c6eaa4f2f375cd25eea5d3d4d6d1f85d5aac8ef1
KamilKoso <kamilkosobudzki1@gmail.com>
2024-03-19 19:53:22 +0100

    Rolled out a new way to download document without paying

--- main.js
+++ main.js
@@ -1,44 +1,41 @@
 const NOT_READY_COLOR = "#c95145";
 const READY_COLOR = "#4caf50";
 
+const captchaDiv = document.getElementById("recap-show");
 const downloadButton = document.getElementById("dwn_btn");
 const pageContent = document.getElementById("content");
 
-async function getDownloadInfo() {
-  const item_id = downloadButton.getAttribute("data-id");
-  const formData = new FormData();
-  formData.append("item_id", item_id);
-  formData.append("rc", Math.floor(Math.random() * 9 + 1));
-
-  const result = await fetch("/start/download", {
-    method: "POST",
-    credentials: "include",
-    body: formData,
-  });
-
-  if (result.status !== 200) {
-    throw new Error(`[Docer Downloader] Couldn't fetch download URL`, result);
+function handleMutations(mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "attributes" && mutation.attributeName === "data-pdf-url") {
+      try {
+        const downloadUrl = mutation.target.getAttribute("data-pdf-url");
+        adjustDownloadButton(downloadUrl);
+      } catch (err) {
+        downloadButton.style.borderColor = NOT_READY_COLOR;
+        throw err;
+      } finally {
+        observer.disconnect();
+      }
+    }
   }
-
-  return (await result.json()).response;
 }
 
-async function main() {
-  try {
-    const downloadInfo = await getDownloadInfo();
-    if(downloadInfo.payment_info === 0) {
-      downloadButton.style.borderColor = READY_COLOR;
-      return; //* no payment required to download this document;
-    }
+function adjustDownloadButton(downloadUrl) {
+  downloadButton.onclick = () => {
+    window.open(downloadUrl, "_blank").focus();
+  };
+  downloadButton.style.borderColor = READY_COLOR;
+}
 
-    downloadButton.onclick = () => {
-      window.open(downloadInfo.url, "_blank").focus();
-    };
-    downloadButton.style.borderColor = READY_COLOR;
-  } catch (err) {
-    downloadButton.style.borderColor = NOT_READY_COLOR;
-    throw err;
-  }
+function main() {
+  console.log(captchaDiv);
+  captchaDiv.innerHTML += "<br>Uzupełnij captche aby pobrać plik bez opłat"
+  const observer = new MutationObserver(handleMutations);
+  observer.observe(document.documentElement, {
+    attributes: true,
+    subtree: true,
+  });
 }
 
 main();
